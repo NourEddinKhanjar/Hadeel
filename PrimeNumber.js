270413118b104483e78575a0c290c62c5717dbd6
Hadeel <hadeelalomary66@gmail.com>
2018-04-09 09:54:26 +0300

    PrimeNumber

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,40 +1,37 @@
 
 
 //find all prime number between 1 - 1000
-var prime=[1];
-for(var i=2;i<=10;i++)
+var array=[];
+var count=0;
+for(var i=1;i<=1000;i++)
 {
-    for(var j=i;j>=2;j--)
+    for(var j=i;j>=1;j--)
     {
-        if(i==j)
-        {
-            prime.push(i);
-        }
-        else
-        {
-            if(i%j==0)
-            {
-                prime.pop();
-            }
-        }
+        if(i%j==0)
+            count++;
     }
+    if(count<=2)
+    {
+       array.push(i);
+    }
+    count=0;
 }
-for(var i=0;i<=prime.length;i++)
+for(var i=0;i<=array.length;i++)
 {
-    console.log(prime[i]);
+    console.log(array[i]);
 }
 
 
-//Add number 5 for each array element
-var array=[1,2,3];
-var n=array[0]+5;
-var outarray=[n]
-for(var i=1;i<=array.length;i++)
-{
-    var m=array[i]+5;
-    outarray.push(m);
-}
-for(var j=0;j<=outarray.length;i++)
-{
-    console.log(outarray[i]);
-}
+// //Add number 5 for each array element
+// var array=[1,2,3];
+// var n=array[0]+5;
+// var outarray=[n]
+// for(var i=1;i<=array.length;i++)
+// {
+//     var m=array[i]+5;
+//     outarray.push(m);
+// }
+// for(var j=0;j<=outarray.length;i++)
+// {
+//     console.log(outarray[i]);
+// }
